856814d137e47e1857f5e04d13fdb787c7ededd2
Mahesh <60826143+maheshmahi457@users.noreply.github.com>
2020-02-09 01:38:10 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,9 @@
 function hello(){
 alert("Hi Guys Let's Start Now");
 }
-hello();
+// hello();
+Hi This new notes 
+Description about the working project
+
+document.body.innerHTML = "<h1>Welcome</h1>";
+

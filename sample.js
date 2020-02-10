b4dfec814d4f7802fd8290275d73db89f30586c5
Mahesh <=>
2020-02-10 19:42:48 +0530

    added some css to index page

--- sample.js
+++ sample.js
@@ -4,6 +4,8 @@ alert("Hi Guys Let's Start Now");
 // hello();
 // Hi This new notes 
 // Description about the working project
+var h1 = "<h1>Welcome to KSM Community</h1>";
+var signup = "<button><a href='signup.html'>Sign Up</a></button>";
+document.body.innerHTML = h1+"<button><a href='login.html'>Sign In</a></button>"+signup;
 
-document.body.innerHTML = "<h1>Welcome</h1>";
 

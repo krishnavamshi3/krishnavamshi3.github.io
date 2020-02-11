08b0fff7b24e189a6a59e82658fb0d57885609ad
Mahesh <=>
2020-02-11 19:46:43 +0530

    save and download json file

--- Auth/register.js
+++ Auth/register.js
@@ -1,9 +1,33 @@
 var arr = {};
-var len=document.getElementsByName('exampleInputPassword1').length;
+var len=document.getElementsByName('input').length;
 function onRegister(){
     for(let i=0;i<len;i++)
     {
-        arr[i] = document.getElementsByName('exampleInputPassword1')[i].value;
+        arr[i] = document.getElementsByName('input')[i].value;
     }
     console.log(JSON.stringify(arr));
+
+
+    file = fopen("c:\MyFile.txt", 3);
+    console.log(file);
+}
+
+"use strict";
+
+function exportToJsonFile() {
+    var file = fopen("data.json", 3);
+    console.log(file);
+    for(let i=0;i<len;i++)
+    {
+        arr[i] = document.getElementsByName('input')[i].value;
+    }
+    let dataStr = JSON.stringify(arr);
+    let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+
+    let exportFileDefaultName = 'data.json';
+
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
 }

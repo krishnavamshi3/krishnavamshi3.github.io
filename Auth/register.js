9ab6ce017b7a6eea6f70a129f2edcda313d7b76a
Mahesh <60826143+maheshmahi457@users.noreply.github.com>
2020-03-08 00:46:07 +0530

    myswl con in node

--- Auth/register.js
+++ Auth/register.js
@@ -12,22 +12,17 @@ function onRegister(){
     console.log(file);
 }
 
-"use strict";
-
-function exportToJsonFile() {
-    var file = fopen("data.json", 3);
-    console.log(file);
-    for(let i=0;i<len;i++)
-    {
-        arr[i] = document.getElementsByName('input')[i].value;
-    }
-    let dataStr = JSON.stringify(arr);
-    let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
-
-    let exportFileDefaultName = 'data.json';
-
-    let linkElement = document.createElement('a');
-    linkElement.setAttribute('href', dataUri);
-    linkElement.setAttribute('download', exportFileDefaultName);
-    linkElement.click();
+function exportToJsonFile(){
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host : "localhost",
+        user : "mahesh",
+        password : "Mahi9652"
+    });
+con.connect(function(err){
+    if(err) throw err;
+    console.log("connected");
+});
 }
+
+exportToJsonFile();
